37e7f664787ec1f0df2429f009ec8a8f38061d71
Manbing <manbing3@gmail.com>
2018-03-09 22:00:44 -0800

    Purpose: 	Modified qemu command.
    Description:

--- programs.user/gpl/qemu/qemu.sh
+++ programs.user/gpl/qemu/qemu.sh
@@ -8,25 +8,25 @@ start)
 ;;
 
 kgbd)
-	qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -nographic -kernel ./arm-image/kernel/zImage -initrd arm-image/rootfs.img -append "root=/dev/ram rdinit=/bin/sh kgdboc=ttyAMA0,115200 kgdbwait"
+	sudo qemu-system-arm -M vexpress-a9 -cpu cortex-a9 -nographic -kernel ./arm-image/kernel/zImage -initrd arm-image/rootfs.img -append "root=/dev/ram rdinit=/bin/sh kgdboc=ttyAMA0,115200 kgdbwait"
 
 # it is okay command
 #qemu-system-arm -M vexpress-a9 -cpu cortex-a9  -m 128M -kernel ./arm-image/kernel/zImage -initrd arm-image/rootfs.img -dtb arm-image/kernel/vexpress-v2p-ca9.dtb -append "root=/dev/ram rdinit=/bin/sh kgdboc=ttyAMA0,115200 kgdbwait"
 ;;
 
 host_bridge)
-	ip address del 192.168.110.132/24 dev ens33
-	ip link add name br0 type bridge
-	ip link set br0 up
-	ip address add 192.168.110.132/24 dev br0
-	ip link set ens33 up
-	ip link set ens33 master br0
-	ip route add  default via 192.168.110.2 dev br0
+	sudo ip address del 192.168.110.132/24 dev ens33
+	sudo ip link add name br0 type bridge
+	sudo ip link set br0 up
+	sudo ip address add 192.168.110.132/24 dev br0
+	sudo ip link set ens33 up
+	sudo ip link set ens33 master br0
+	sudo ip route add  default via 192.168.110.2 dev br0
 ;;
 
 qemu_bridge)
-	ip link set tap0 up
-	ip link set tap0 master br0
+	sudo ip link set tap0 up
+	sudo ip link set tap0 master br0
 ;;
 
 *)
