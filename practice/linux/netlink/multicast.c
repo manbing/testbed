5bf92ef4dc6946e7684479a805e1ef23ae22b963
Manbing <manbing_chou@inndatw.com>
2019-08-12 00:59:59 +0800

    Using workqueue to send netlink multicast message.

--- practice/linux/netlink/multicast.c
+++ practice/linux/netlink/multicast.c
@@ -5,30 +5,32 @@
 #include <linux/types.h>
 #include <net/netlink.h>
 #include <net/sock.h>
+#include <linux/workqueue.h>
 
 #define MY_GRP (1 << 1)
 #define MY_CHANNEL (31)
 
 struct sock *nl_sk = NULL;
-//static void tl_f(unsigned long val);
-static void send_msg(unsigned long val);
-static DECLARE_TASKLET(tl, send_msg, 0);
+static void send_msg(struct work_struct *work);
+//static DECLARE_TASKLET(tl, send_msg, 0);
+struct work_struct work;
+static atomic_t run = ATOMIC_INIT(1);
 
-static void send_msg(unsigned long val)
+static void send_msg(struct work_struct *work)
 {
     struct sk_buff *skb = NULL;
     char msg [64] = "Message from Linux kernel";
     int size = 0, ret = 0;
     struct nlmsghdr *nlh = NULL;
 
-    while (1) {
+    while (atomic_read(&run)) {
         msleep(3000);
         size = sizeof(msg);
         skb = nlmsg_new(size, GFP_KERNEL);
 
         if (!skb) {
             printk(KERN_ERR "%s: Cannot create netlink message\n", __func__);
-            return -ENOMEM;
+            return;
         }
 
         nlh = nlmsg_put(skb, 0, 1, NLMSG_DONE, size, 0);
@@ -43,7 +45,7 @@ static void send_msg(unsigned long val)
 
         printk("%s: Send message successfully\n", __func__);
     }
-    return 0;
+    return;
 }
 
 static int __init init(void)
@@ -59,15 +61,26 @@ static int __init init(void)
         return -ENOMEM;
     }
 
-    tasklet_schedule(&tl);
+    /* Tasklet */
+    //tasklet_schedule(&tl);
+
+    /* Workqueue */
+    INIT_WORK(&work, send_msg);
+    schedule_work(&work);
     return 0;
 }
 
 static void __exit exit(void)
 {
     netlink_kernel_release(nl_sk);
-    tasklet_disable(&tl);
-    tasklet_kill(&tl);
+    
+    /* Tasklet */
+    //tasklet_disable(&tl);
+    //tasklet_kill(&tl);
+    
+    /* Workqueue */
+    atomic_set(&run, 0);
+    cancel_work_sync(&work);
 }
 
 module_init(init);
