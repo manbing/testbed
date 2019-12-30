897626337d877ff71db573a502a68bb5c7be74ff
Manbing <manbing_chou@inndatw.com>
2019-12-31 00:30:01 +0800

    kgdb debug Linux kernel over Qemu.

--- programs.user/gpl/qemu/qemu_x86.sh
+++ programs.user/gpl/qemu/qemu_x86.sh
@@ -3,7 +3,15 @@ echo [$0][$1]
 
 case $1 in
 start)
-	sudo qemu-system-x86_64 -smp 2 -m 2G -kernel ./image/bzImage -initrd ./image/rootfs.img -append "root=/dev/ram rdinit=/sbin/init console=ttyS0" -nographic -net nic -net tap,ifname=tap0,script=no
+	sudo qemu-system-x86_64 -smp 2 -m 1G -kernel ./image/bzImage -initrd ./image/rootfs.img -append "root=/dev/ram rdinit=/sbin/init console=ttyS0" -nographic -net nic -net tap,ifname=tap0,script=no
+;;
+
+kgdb)
+	sudo qemu-system-x86_64 -smp 2 -m 1G -kernel ./image/bzImage -initrd ./image/rootfs.img -serial tcp::1234,server,nowait -append "root=/dev/ram rdinit=/sbin/init kgdbwait kgdboc=ttyS0,115200" -net nic -net tap,ifname=tap0,script=no
+;;
+
+gdb)
+	sudo qemu-system-x86_64 -smp 2 -m 1G -kernel ./image/bzImage -initrd ./image/rootfs.img -serial stdio -append "root=/dev/ram rdinit=/sbin/init" -S -gdb tcp::1234
 ;;
 
 *)
